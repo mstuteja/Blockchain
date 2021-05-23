5602af02c62951c5e6b67aa9c81ab298518e7a18
Manmeet Singh Tuteja <mstuteja2002@gmail.com>
2021-05-24 03:06:06 +0530

    Minor changes

--- blockchain/block.js
+++ blockchain/block.js
@@ -1,35 +1,44 @@
 const SHA256 = require('crypto-js/sha256');
+const DIFFICULTY = 4;
 
 class Block{
-    constructor(timestamp, lastHash, hash, data){
+    constructor(timestamp, lastHash, hash, data, nonce){
         this.timestamp=timestamp;
         this.lastHash=lastHash;
         this.hash=hash;
-        this.data=data
+        this.data=data;
+        this.nonce=nonce;
     }
 
     toString(){
-        return '\nBlock details:\n\nTimeStamp: '+ this.timestamp+ '\nLastHash:' + this.lastHash.substring(0,30)+ '\nHash:' + this.hash.substring(0,30)+ '\nData:' + this.data+"\n";
+        return '\nBlock details:\n\nTimeStamp: '+ this.timestamp+ '\nLastHash:' + this.lastHash.substring(0,30)+ '\nHash:' + this.hash.substring(0,30)+ '\nData:' + this.data+"\n"+ '\Nonce:' + this.nonce+"\n";
     }
 
     static genesis(){
-        return new this('hello','yo','he','hahahahahah');
+        return new this('Gensis Start','---','anythingrandom','random',0);
     }
     
     static mineBlock(lastBlock, data){
-        const timestamp = Date.now();
+        let timestamp;
         const lastHash = lastBlock.hash;
-        const hash = Block.hash(timestamp,lastHash,data);
-        return new this(timestamp,lastHash,hash,data);
+        let nonce,hash;
+        nonce=0;
+        do{
+            timestamp = Date.now();
+            nonce++;
+            hash = Block.hash(timestamp,lastHash,data);
+        }while(hash.substring(o,DIFFICULTY)!== '0'.repeat(DIFFICULTY));
+    
+        return new this(timestamp,lastHash,hash,data,nonce);
     }
 
-    static hash(timestamp,lastHash,data){
-        return SHA256(`${timestamp}${lastHash}${data}`).toString();
+    static hash(timestamp,lastHash,data,nonce){
+        return SHA256(`${timestamp}${lastHash}${data}${nonce}`).toString();
     }
 
     static blockHash(block){
-        const {timestamp, lastHash, data} = block;
-        return Block.hash(timestamp,lastHash,data);
+        const {timestamp, lastHash, data, nonce} = block;
+        return Block.hash(timestamp,lastHash,data, nonce);
     }
 
 }
